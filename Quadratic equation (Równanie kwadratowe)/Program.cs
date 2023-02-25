6b3fb59840ab2225cce6fd5a097628cd9dd65362
Ruslan Pidhainy <ruslanpidhainyi02@gmail.com>
2023-02-25 21:34:45 +0100

    Ex3 (Równanie kwadratowe)

--- Quadratic equation (Równanie kwadratowe)/Program.cs
+++ Quadratic equation (Równanie kwadratowe)/Program.cs
@@ -23,26 +23,27 @@ class Program
         double D = Math.Pow(b, 2) - 4 * a * c;
 
 
-        if (D > 0)
+        if (a == 0 && b == 0 && c == 0)
         {
-            double x1 = ((-b) - Math.Sqrt(D)) / (2 * a);
-            double x2 = ((-b) + Math.Sqrt(D)) / (2 * a);
-            Console.WriteLine("x1={0:N2}", x1);
-            Console.WriteLine("x2={0:N2}", x2);
-        }
-        else if (D == 0)
-        {
-            double x = ((-b) + Math.Sqrt(D)) / (2 * a);
-            Console.WriteLine("x={0:N2}", x);
+            Console.WriteLine("infinity");
         }
         else if (D < 0)
         {
             Console.WriteLine("empty");
         }
-        else if (a == 0 & b == 0 & c == 0)
+        else if (Math.Round(D) > 0)
         {
-            Console.WriteLine("infinity");
+            double x1 = (-b - Math.Sqrt(D)) / 2 * a;
+            double x2 = (-b + Math.Sqrt(D)) / 2 * a;
+            Console.WriteLine("x1={0:N2}", x1);
+            Console.WriteLine("x2={0:N2}", x2);
         }
+        else if (Math.Round(D) == 0)
+        {
+            double x = (-b + Math.Sqrt(Math.Round(D))) / 2 * a;
+            Console.WriteLine("x={0:N2}", x);
+        }
+
     }
 }
 
